c99c89a9fc836e4bd8d0f1de8cddc0412a09f6ae
Benjamin Massey <benjamin.w.massey@gmail.com>
2022-11-08 12:24:10 -0800

    Have encryption now, and oauth in password mode

--- bsd.py
+++ bsd.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import *
+from cryptography.fernet import Fernet
 import os
 
 # --- DATA INITIALIZATION --
@@ -7,6 +8,7 @@ data_dir = "./data/"
 chat_path = data_dir + "chat.txt"
 bot_path = data_dir + "bot.txt"
 oauth_path = data_dir + "oauth.txt"
+key_path = data_dir + "private-key.txt"
 
 if not os.path.isdir(data_dir):
     os.mkdir(data_dir, 0o666)
@@ -15,6 +17,31 @@ for path in [chat_path, bot_path, oauth_path]:
     if not os.path.isfile(path):
         file = open(path, 'w')
         file.close()
+        
+key = None
+
+if os.path.isfile(key_path):
+    file = open(key_path, 'r')
+    key = file.read()
+    file.close
+else:
+    key = Fernet.generate_key()
+    file = open(key_path, 'w')
+    file.write(key.decode("utf-8"))
+    file.close()
+    
+fernet = Fernet(key)
+
+# --- FUNCTIONS ---
+
+def fencrypt(text):
+    global fernet
+    text = fernet.encrypt(text.encode())
+    return text.decode("utf-8")
+
+def fdecrypt(text):
+    global fernet
+    return fernet.decrypt(text).decode()
 
 # --- GUI INITIALIZATION ---
 
@@ -26,9 +53,12 @@ layout = QVBoxLayout()
 
 # --- GUI FUNCTIONS ---
 
-def writeEntryToPath(entry, path):
+def writeEntryToPath(entry, path, encrypt=False):
     file = open(path, 'w')
-    file.write(entry.text())
+    text = entry.text()
+    if encrypt:
+        text = fencrypt(text)
+    file.write(text)
     file.close()
 
 # --- CHAT CHANNEL ---
@@ -36,7 +66,11 @@ def writeEntryToPath(entry, path):
 chat_label = QLabel("Chat Channel:")
 layout.addWidget(chat_label)
 
-chat_entry = QLineEdit()
+file = open(chat_path, 'r')
+chat_saved = file.read()
+file.close()
+
+chat_entry = QLineEdit(chat_saved)
 layout.addWidget(chat_entry)
 
 chat_button = QPushButton("Submit")
@@ -51,7 +85,11 @@ chat_button.clicked.connect(
 bot_label = QLabel("Bot Username:")
 layout.addWidget(bot_label)
 
-bot_entry = QLineEdit()
+file = open(bot_path, 'r')
+bot_saved = file.read()
+file.close()
+
+bot_entry = QLineEdit(bot_saved)
 layout.addWidget(bot_entry)
 
 bot_button = QPushButton("Submit")
@@ -61,20 +99,20 @@ bot_button.clicked.connect(
     lambda: writeEntryToPath(bot_entry, bot_path)
 )
 
-
 # --- OAUTH ---
 
 oauth_label = QLabel("Bot Oauth Token:")
 layout.addWidget(oauth_label)
 
 oauth_entry = QLineEdit()
+oauth_entry.setEchoMode(QLineEdit.EchoMode.Password)
 layout.addWidget(oauth_entry)
 
 oauth_button = QPushButton("Submit")
 layout.addWidget(oauth_button)
 
 oauth_button.clicked.connect(
-    lambda: writeEntryToPath(oauth_entry, oauth_path)
+    lambda: writeEntryToPath(oauth_entry, oauth_path, True)
 )
 
 # --- FINALIZATION ---
